d0952d5f8040c459f497d25dd48eb09ceb899e61
PhantomShift <tommeh2002@gmail.com>
2021-05-16 01:35:42 -0400

    Fixed typecheck error

--- src/Vector2.py
+++ src/Vector2.py
@@ -86,7 +86,7 @@ class Vector2:
             angle = math.cos(angle)
         return Vector2.from_angle(math.atan2(self.y, self.x) + angle, self.magnitude)
 
-    def move_toward(self, destination: Vector2, delta):
+    def move_toward(self, destination, delta):
         """inspired by Godot Vector2's move_toward method"""
         if destination == self:
             return self
